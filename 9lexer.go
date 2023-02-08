5f6b710ac04dbd59e4c08729c109e53be3610383
isabelle-sanford <isabellesanford@gmail.com>
2023-02-08 00:26:20 -0500

    add and debug basic test for lexer

--- 9lexer.go
+++ 9lexer.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"strconv"
 	"strings"
 	"text/scanner"
@@ -11,7 +13,7 @@ var keywords map[string]bool = map[string]bool{"select": true, "from": true, "wh
 type Lexer struct {
 	keywords map[string]bool // might need map
 	tok      scanner.Scanner
-	currTok  rune
+	currTok  rune // maybe unnecessary tbh
 }
 
 func makeLexer(s string) Lexer {
@@ -23,10 +25,11 @@ func makeLexer(s string) Lexer {
 	return Lexer{keywords, tok, currTok}
 }
 
-func (l *Lexer) matchDelim(d rune) bool { // int should be char
-	return l.tok.Peek() == d // honestly no idea if that works
+func (l *Lexer) matchDelim(d rune) bool {
+	return l.tok.TokenText() == string(d) // honestly no idea if that works
 }
 func (l *Lexer) matchIntConstant() bool {
+	//fmt.Println("Checking ", l.tok.TokenText(), " for integer")
 	_, err := strconv.Atoi(l.tok.TokenText())
 	if err == nil {
 		return true
@@ -40,9 +43,13 @@ func (l *Lexer) matchKeyword(w string) bool {
 	return l.tok.TokenText() == w
 }
 func (l *Lexer) matchId() bool {
+	//fmt.Println("checking ", l.tok.TokenText(), " for Id")
 	_, ok := l.keywords[l.tok.TokenText()]
+	if l.matchIntConstant() {
+		return false
+	}
 	// todo check that it's actually a word
-	return ok
+	return !ok
 }
 
 func (l *Lexer) eatDelim(d rune) (ok bool) {
@@ -105,16 +112,62 @@ func (l *Lexer) nextToken() {
 }
 
 // page 243
-func LexerTest() {
+func main() {
 	x := ""
 	y := 0
+	ok := true
+
+	src := "A = 1\n3 = B"
+
+	sc := bufio.NewScanner(strings.NewReader(src))
+
+	for sc.Scan() {
+		lex := makeLexer(sc.Text())
+		fmt.Println("looking at line ", sc.Text())
+		if lex.matchId() {
+			x, ok = lex.eatId()
+			if !ok {
+				fmt.Println("thinks first part is Id but does not get it")
+			}
+			lex.eatDelim('=')
+			y, ok = lex.eatIntConstant()
+			if !ok {
+				fmt.Println("thinks second part is int constant but does not get it")
+			}
+		} else {
+			y, ok = lex.eatIntConstant()
+			if !ok {
+				fmt.Println("thinks first part is int constant but does not get it")
+			}
+			lex.eatDelim('=')
+			x, ok = lex.eatId()
+			if !ok {
+				fmt.Println("thinks second part is Id but does not get it")
+			}
+		}
+		fmt.Println(x, " equals ", y)
+	}
+
+	// a := "A = 1"
+	// delim := '='
+
+	// var tok scanner.Scanner
+	// tok.Init(strings.NewReader(a))
+	// tok.Scan()
+
+	// // print and eat id (A)
+	// fmt.Println("first token:", tok.TokenText())
+	// tok.Scan()
+
+	// tt := tok.TokenText()
+	// fmt.Println("second token:", tok.TokenText())
+	// fmt.Println("with peek:", tok.Peek())
+	// fmt.Println("with next:", tok.Next())
+
+	// fmt.Println("delimiter:", delim)
+	// fmt.Println("delimiter as string:", string(delim))
+
+	// fmt.Println("token contains delim rune?", strings.ContainsRune(tt, delim))
+	// fmt.Println("token equal to string(delim)?", tt == string(delim))
 
 }
-
-// func initKeywords() []string {
-// 	keywords := []string{"select","from","where","and","insert","into","values","delete",
-// 		"update","set","create","table","varchar","int","view","as","index","on"}
-// 		return keywords
-// }
-
-// PRED PARSER
